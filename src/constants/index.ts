6a2b1742011d01ce42535a22db417c0a66e627e7
Çağlayan <demircicaglayan13@gmail.com>
2025-01-16 01:20:54 +0300

    create constants includes objects and arrays like urls, navigation tabs, footer liks, due to keep the project modular

---
+++ src/constants/index.ts
@@ -0,0 +1,25 @@
+export const urls = {
+  instagram: '',
+  xTwitter: '',
+  linledIn: '',
+  gitHub: '',
+  // other account urls to share on pages 
+}
+
+export const navigation = [
+  { label: 'Home', path: '/' },
+  { label: 'About', path: '/' },
+  { label: 'Services', path: '/' },
+  { label: 'Contact', path: '/' },
+  { label: 'Blog', path: '/' },
+  // other navbar items
+]
+
+export const footerLinks = [
+  { label: 'About', path: '/' },
+  { label: 'Service 1', path: '/' },
+  { label: 'Service 2', path: '/' },
+  { label: 'Contact', path: '/' },
+  { label: 'Blog', path: '/' },
+  // other footer items
+]
